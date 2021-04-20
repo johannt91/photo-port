0ed74b2b47a0e6b1a4355ae459f4f910bc461dcd
Johann Taylor <j.taylor1343@gmail.com>
2021-04-19 22:42:33 -0400

    Created tests for About section

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+afterEach(cleanup); //cleanup leftover memory data that could give false results
+
+
+describe('About component', () => { // declare component that is being tested
+    // First Test
+    it('renders', () => {
+        render(<About />);
+    });
+    // Second Test
+    it('mathces snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot(); // expect() is used with a matcher to assert something about a value
+    })
+})
