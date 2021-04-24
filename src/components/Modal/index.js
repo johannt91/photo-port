db2bc80ccb4921e4c7ac1a9ea4b602612a792641
Johann Taylor <j.taylor1343@gmail.com>
2021-04-24 00:31:43 -0400

    Added modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Modal({onClose, currentPhoto}) {
+    const { name, category, description, index } = currentPhoto;
+    return (
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">{name}</h3>
+                <img src={require(`../../assets/small/${category}/${index}.jpg`).default} alt="current category"/>
+                <p>{description}</p>
+                <button onClick={onClose} type="button">
+                Close this modal
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
